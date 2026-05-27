a217b2665514b40fe18b2d9c17427e9086899c61
minzhoo2206-alt <minzhoo2206@gmail.com>
2026-05-28 02:24:07 +0700

    data chiTietSP

--- Views/pages/chi-tiet-san-pham.php
+++ Views/pages/chi-tiet-san-pham.php
@@ -4,97 +4,97 @@
         [
             "name" => "Nhẫn cưới nam Kim cương Vàng 75% (18K) PNJ Chung Đôi",
             "image" => "https://cdn.pnj.io/images/thumbnails/485/485/detailed/180/sp-GNDD00C000074-nhan-cuoi-nam-kim-cuong-vang-18k-pnj-chung-doi-1.png",
-            "price" => "13 731 000"
+            "price" => "13 731 000 ₫"
         ],
         [
             "name" => "Nhẫn cưới Kim cương Vàng 75% (18K) PNJ",
             "image" => "https://cdn.pnj.io/images/thumbnails/485/485/detailed/179/sp-GNDD00C000081-nhan-cuoi-kim-cuong-vang-18k-pnj-chung-doi-1.png",
-            "price" => "9 511 000"
+            "price" => "9 511 000 ₫"
         ],
         [
-            "name" => "",
-            "image" => "",
-            "price" => "13 731 000"
+            "name" => "Nhẫn cưới Kim cương Vàng trắng 58,5% (14K) PNJ",
+            "image" => "https://cdn.pnj.io/images/thumbnails/485/485/detailed/180/sp-GNDD00W000269-nhan-cuoi-kim-cuong-vang-trang-14k-pnj-1.png",
+            "price" => "20.023.000 ₫"
         ],
         [
-            "name" => "",
-            "image" => "",
-            "price" => "13 731 000"
+            "name" => "Nhẫn nam Kim cương Vàng trắng 58,5% (14K) MANCODE by PNJ",
+            "image" => "https://cdn.pnj.io/images/thumbnails/485/485/detailed/224/sp-gnddddw013248-nhan-kim-cuong-vang-trang-14k-pnj-1.png",
+            "price" => "127.316.000 ₫"
         ],
         [
-            "name" => "",
-            "image" => "",
-            "price" => "13 731 000"
+            "name" => "Nhẫn nam Kim cương Vàng trắng 58,5% (14K) MANCODE by PNJ",
+            "image" => "https://cdn.pnj.io/images/thumbnails/485/485/detailed/216/sp-gnddddw013360-nhan-nam-kim-cuong-vang-trang-18k-pnj-1.png",
+            "price" => "162.577.000 ₫"
         ],
         [
-            "name" => "",
-            "image" => "",
-            "price" => "13 731 000"
+            "name" => "Nhẫn nam Kim cương Vàng trắng 58,5% (14K) MANCODE by PNJ | BST Mancode",
+            "image" => "https://cdn.pnj.io/images/thumbnails/485/485/detailed/223/sp-gndd00w005729-nhan-kim-cuong-vang-trang-14k-pnj-1.png",
+            "price" => "51.651.000 ₫"
         ],
         [
-            "name" => "",
-            "image" => "",
-            "price" => "13 731 000"
+            "name" => "Nhẫn nam Kim cương Vàng trắng 58,5% (14K) MANCODE by PNJ",
+            "image" => "https://cdn.pnj.io/images/thumbnails/485/485/detailed/230/sp-gnddddw013067-nhan-kim-cuong-vang-trang-14k-mancode-by-pnj-1.png",
+            "price" => "115.643.000 ₫"
         ],
         [
-            "name" => "",
-            "image" => "",
-            "price" => "13 731 000"
+            "name" => "Nhẫn nam Kim cương Vàng trắng 58,5% (14K) MANCODE by PNJ",
+            "image" => "https://cdn.pnj.io/images/thumbnails/485/485/detailed/219/sp-gnddddw013287-nhan-nam-kim-cuong-vang-trang-14k-pnj-1.png",
+            "price" => "87.848.000 ₫"
         ],
         [
-            "name" => "",
-            "image" => "",
-            "price" => "13 731 000"
+            "name" => "Vỏ nhẫn nam Kim cương Vàng trắng 75% (18K) MANCODE by PNJ",
+            "image" => "https://cdn.pnj.io/images/thumbnails/485/485/detailed/134/gn00ddw000680-vo-nhan-nam-kim-cuong-vang-trang-18k-pnj-001.png",
+            "price" => "78.501.000 ₫"
         ],
         [
-            "name" => "",
-            "image" => "",
-            "price" => "13 731 000"
+            "name" => "Vỏ nhẫn nam Kim cương Vàng trắng 75% (18K) MANCODE by PNJ",
+            "image" => "https://cdn.pnj.io/images/thumbnails/485/485/detailed/134/gn00ddw000680-vo-nhan-nam-kim-cuong-vang-trang-18k-pnj-001.png",
+            "price" => "78.501.000 ₫"
         ],
         [
-            "name" => "",
-            "image" => "",
-            "price" => "13 731 000"
+            "name" => "Nhẫn nam Kim cương Vàng trắng 58,5% (14K) MANCODE by PNJ",
+            "image" => "https://cdn.pnj.io/images/thumbnails/485/485/detailed/196/sp-gnddddw000366-nhan-kim-cuong-vang-trang-14k-pnj-1.png",
+            "price" => "78.327.000 ₫"
         ],
         [
-            "name" => "",
-            "image" => "",
-            "price" => "13 731 000"
+            "name" => "Nhẫn nam Kim cương Vàng trắng 58,5% (14K) MANCODE by PNJ",
+            "image" => "https://cdn.pnj.io/images/thumbnails/485/485/detailed/176/sp-GNDDDDW000265-nhan-nam-kim-cuong-vang-trang-14k-pnj-01.png",
+            "price" => "62.430.000 ₫"
         ],
         [
-            "name" => "",
-            "image" => "",
-            "price" => "13 731 000"
+            "name" => "Nhẫn nam Kim cương Vàng trắng 58,5% (14K) MANCODE by PNJ DDDDW002745",
+            "image" => "https://cdn.pnj.io/images/thumbnails/485/485/detailed/196/sp-gnddddw002745-nhan-kim-cuong-vang-trang-14k-pnj-1.png",
+            "price" => "108.737.000 ₫"
         ],
         [
-            "name" => "",
-            "image" => "",
-            "price" => "13 731 000"
+            "name" => "Vỏ nhẫn nam Kim cương Vàng trắng 75% (18K) MANCODE by PNJ",
+            "image" => "https://cdn.pnj.io/images/thumbnails/485/485/detailed/190/sp-gn00ddw002044-vo-nhan-nam-kim-cuong-vang-trang-18k-pnj-1.png",
+            "price" => "70.849.000 ₫"
         ],
         [
-            "name" => "",
-            "image" => "",
-            "price" => "13 731 000"
+            "name" => "Vỏ nhẫn nam Kim cương Vàng trắng 75% (18K) MANCODE by PNJ 00DDW002046",
+            "image" => "https://cdn.pnj.io/images/thumbnails/485/485/detailed/190/sp-gn00ddw002046-vo-nhan-nam-kim-cuong-vang-trang-18k-pnj-1.png",
+            "price" => "116.919.000 ₫"
         ],
         [
-            "name" => "",
-            "image" => "",
-            "price" => "13 731 000"
+            "name" => "Nhẫn nam Kim cương Vàng trắng 58,5% (14K) MANCODE by PNJ",
+            "image" => "https://cdn.pnj.io/images/thumbnails/485/485/detailed/107/gnddddw004723-nhan-nam-kim-cuong-vang-trang-14k-pnj.png",
+            "price" => "56.373.000 ₫"
         ],
         [
-            "name" => "",
-            "image" => "",
-            "price" => "13 731 000"
+            "name" => "Nhẫn nam Kim cương Vàng trắng 58,5% (14K) MANCODE by PNJ DDDDW002658",
+            "image" => "https://cdn.pnj.io/images/thumbnails/485/485/detailed/196/sp-gnddddw002658-nhan-vang-trang-14k-kim-cuong-pnj-1.png",
+            "price" => "65.043.000 ₫"
         ],
         [
-            "name" => "",
-            "image" => "",
-            "price" => "13 731 000"
+            "name" => "Nhẫn nam Kim cương Vàng 58,5% (14K) MANCODE by PNJ",
+            "image" => "https://cdn.pnj.io/images/thumbnails/485/485/detailed/199/sp-gnddddc001708-nhan-nam-kim-cuong-vang-14k-pnj-1.png",
+            "price" => "54.494.000 ₫"
         ],
         [
-            "name" => "",
-            "image" => "",
-            "price" => "13 731 000"
+            "name" => "Nhẫn nam Kim cương Vàng 75% (18K) MANCODE by PNJ | BST Mancode",
+            "image" => "https://cdn.pnj.io/images/thumbnails/485/485/detailed/226/sp-gnddddc001891-nhan-kim-cuong-vang-18k-mancode-by-pnj-1.png",
+            "price" => "114.384.000 ₫"
         ],
     ];
     ?>
